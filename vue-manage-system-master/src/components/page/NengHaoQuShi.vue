14b9346f1aff01466091ca80d7b3637016b7dd87
shihongyugg <1445300018@qq.com>
2018-12-23 20:06:55 +0800

    shy-2018-12-23-(6)

--- vue-manage-system-master/src/components/page/NengHaoQuShi.vue
+++ vue-manage-system-master/src/components/page/NengHaoQuShi.vue
@@ -228,43 +228,7 @@
                 compareVal: "tongbi",
                 compareShow: false,
                 dataShow: false,
-                tableData6: [
-                    {
-                        id: "12987122",
-                        name: "王小虎",
-                        amount1: "234",
-                        amount2: "3.2",
-                        amount3: 10
-                    },
-                    {
-                        id: "12987123",
-                        name: "王小虎",
-                        amount1: "165",
-                        amount2: "4.43",
-                        amount3: 12
-                    },
-                    {
-                        id: "12987124",
-                        name: "王小虎",
-                        amount1: "324",
-                        amount2: "1.9",
-                        amount3: 9
-                    },
-                    {
-                        id: "12987125",
-                        name: "王小虎",
-                        amount1: "621",
-                        amount2: "2.2",
-                        amount3: 17
-                    },
-                    {
-                        id: "12987126",
-                        name: "王小虎",
-                        amount1: "539",
-                        amount2: "4.1",
-                        amount3: 15
-                    }
-                ],
+
                 tableData: [],
                 lists: [
                     {
@@ -1633,6 +1597,7 @@
                             font-size: 12px;
                             font-family: HiraginoSansGB-W3;
                             font-weight: bold;
+                            
                             color: rgba(45, 243, 255, 1);
                             option {
                                 background-color: #0b3f6f;
